73f918b56d294c1ad759134b0798630d525dffaa
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-03-18 11:50:46 -0300

    exercicio sobre variaveis

---
+++ ex1.py
@@ -0,0 +1,13 @@
+nome = 'jupita'
+sobrenome = 'carlos'
+idade = 12
+ano_nascimento = 2022 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.20
+
+print("", nome)
+print("", sobrenome)
+print("", idade)
+print("", ano_nascimento)
+print("", maior_de_idade)
+print("", altura_metros)
