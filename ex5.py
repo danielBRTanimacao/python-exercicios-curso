fdef5a878b03f00673f5cfcc9fc47310d7777d2b
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-03-23 17:09:08 -0300

    update ex 5

--- ex5.py
+++ ex5.py
@@ -2,7 +2,8 @@
 Faça um programa que peça ao usuário para digitar um número inteiro,
 informe se este número é par ou ímpar. Caso o usuário não digite um número
 inteiro, informe que não é um número inteiro.
-
+"""
+"""
 try:
     numero = int(input("Digite um número inteiro: "))
 except:
@@ -12,13 +13,22 @@ else:
         print("\033[32mSim\033[m, esse número e \033[32mPAR\033[m!")
     else:
         print("\033[31mNão\033[m, esse número é \033[31mIMPAR\033[m... e não par")
+
 """
 """
 Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
 descrito, exiba a saudação apropriada. Ex. 
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
-
+"""
+hora = float(input("Digite as horas: "))
+if hora == 0 or hora <= 11:
+    print("Bom dia!")
+elif hora == 12 or hora <= 17:
+    print("Boa tarde!")
+elif hora == 18 or 23:
+    print("Boa noite!")
+"""
 """
 Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
