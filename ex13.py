1a399c30b9be5fe3ccf0d22fe3e790e2dce9531e
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-04-17 11:03:26 -0300

    exercicio

---
+++ ex13.py
@@ -0,0 +1,10 @@
+#def par_impar(num):
+#    if num % 2 == 0:
+#        print('Esse número é PAR!')
+#    else:
+#        print("Este número é IMPAR")
+def par_impar(num):
+    return num % 2 == 0
+
+
+print(par_impar(6))
