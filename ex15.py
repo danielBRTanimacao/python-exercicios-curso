978ea4ac7845819ada21f5846cb69cc28ba202ec
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-05-05 18:36:49 -0300

    update exercicio

--- ex15.py
+++ ex15.py
@@ -20,15 +20,19 @@ perguntas = [
 certo = 0
 for per in perguntas:
     print(f"Pergunta {per['pergunta']}\n")
+
     for num, op in enumerate(per['opções']):
         print(f"{num}) {op}")
+
     resp = input(f"Escolha alternativa: ")
+
     if resp == per['resposta']:
         print("\033[32mAcertou\033[m\n")
         certo += 1
     else:
         print("\033[31mErrou\033[m\n")
+
 system('cls')
 print("="*42)
-print(f"Parabens você acertou {certo}!".center(42))
+print(f"Parabens você acertou {certo} de {len(perguntas)} de três perguntas".center(42))
 print("="*42)
