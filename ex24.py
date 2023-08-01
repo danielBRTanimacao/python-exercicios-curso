d6614d3e9d94af442e7953668f140806703a0414
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-08-01 13:06:03 -0300

    minha solução

--- ex24.py
+++ ex24.py
@@ -7,3 +7,23 @@
 # - Crie a data do empréstimo
 # - Crie a data do final do empréstimo
 # - Mostre todas as datas de vencimento e o valor de cada parcela
+
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+total_value = 1000000
+loan_date = datetime(20, 12, 2020) #primeira data do emprestimo
+loan_time = relativedelta(years=5)
+final_date = loan_date + loan_time #data inicial mais os anos de pagamento
+
+
+installment_date = loan_date
+total_installment_dates = [] #lista contem datas/vencimentos
+
+while installment_date < final_date:
+    total_installment_dates.append(installment_date)
+    installment_date += relativedelta(months=1)
+
+for datas in total_installment_dates:
+    ...
+
