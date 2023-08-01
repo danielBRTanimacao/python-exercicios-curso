05292b2ddd17398badb2b941b9d33fedd034ba0b
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-08-01 12:36:10 -0300

    minha solução

---
+++ ex24.py
@@ -0,0 +1,9 @@
+# Exercício solucionado: calculando as datas e parcelas de um empréstimo
+# Maria pegou um empréstimo de 1.000.000
+# para realizar o pagamento em 5 anos.
+# A data em que ela pegou o empréstimo foi
+# 20/12/2020 e o vencimento de cada parcela
+# é no dia 20 de cada mês.
+# - Crie a data do empréstimo
+# - Crie a data do final do empréstimo
+# - Mostre todas as datas de vencimento e o valor de cada parcela
